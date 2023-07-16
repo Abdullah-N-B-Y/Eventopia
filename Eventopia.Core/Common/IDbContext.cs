90ea30a0b36685063a4ea5532556bb8f660db35f
Abdullah N Bani Yassin <anby.githup@gmail.com>
2023-07-16 07:55:26 +0300

    1- add new comment

--- Eventopia.Core/Common/IDbContext.cs
+++ Eventopia.Core/Common/IDbContext.cs
@@ -6,6 +6,7 @@ namespace Eventopia.Core.Common
    //comment to test
    //comment to test
    //comment to test
+   //add new comment
         public interface IDbContext
         {
             public DbConnection Connection { get; }
