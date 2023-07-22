d45c193bb60b00f71b25550ad8aae5e5881540d2
Abdullah N Bani Yassin <anby.githup@gmail.com>
2023-07-22 21:46:55 +0300

    1- Delete repeted code

--- Eventopia/Program.cs
+++ Eventopia/Program.cs
@@ -44,10 +44,6 @@ public class Program
         builder.Services.AddScoped<IUserService, UserService>();
 		builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 		builder.Services.AddScoped<IAuthService, AuthService>();
-		builder.Services.AddScoped<IUserService, UserService>();
-		builder.Services.AddScoped<IAdminService, AdminService>();
-		builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-		builder.Services.AddScoped<IAuthService, AuthService>();
 
 
 
