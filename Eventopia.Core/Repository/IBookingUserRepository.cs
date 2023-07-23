328b4175b2b9bd10f9b3aabd4c59cc37b60d2600
Abdullah N Bani Yassin <anby.githup@gmail.com>
2023-07-23 18:15:15 +0300

    1- Create BookingUser (Repository, Service)

---
+++ Eventopia.Core/Repository/IBookingUserRepository.cs
@@ -0,0 +1,10 @@
+﻿
+using Eventopia.Core.Data;
+
+namespace Eventopia.Core.Repository;
+
+public interface IBookingUserRepository
+{
+    void AddUserToBooking(Booking booking);
+    void DeleteUserFromBooking(int userId, int eventId);
+}
