3129f123587a457237eefd4cf5e79d5ebaa0b4f0
SumWutChill <mramzey@ymail.com>
2023-07-24 09:10:41 +0300

    Fixed authorization policy

--- Eventopia.Core/DTO/UpdatePasswordDTO.cs
+++ Eventopia.Core/DTO/UpdatePasswordDTO.cs
@@ -5,13 +5,19 @@ namespace Eventopia.Core.DTO;
 
 public class UpdatePasswordDTO
 {
-    [Required(ErrorMessage = "Password is required")]
-    public string? OldPassword { set; get; }
+	[Required(ErrorMessage = "old password is required")]
+	[StringLength(50, MinimumLength = 8, ErrorMessage = "Old password must be at least 8 characters and less than 50")]
+	[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$",
+			ErrorMessage = "Old password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character")]
+	public string? OldPassword { set; get; }
 
-    [Required(ErrorMessage = "Password is required")]
-    [StringLength(50, MinimumLength = 3, ErrorMessage = "Password must be between 3 and 50 characters")]
-    public string? NewPassword { set; get; }
+	[Required(ErrorMessage = "New password is required")]
+	[StringLength(50, MinimumLength = 8, ErrorMessage = "New password must be at least 8 characters and less than 50")]
+	[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$",
+			ErrorMessage = "New password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character")]
+	public string? NewPassword { set; get; }
 
-    [Required(ErrorMessage = "Password is required")]
-    public string? ConfirmPassword { set; get; }
+	[Required(ErrorMessage = "Password is required")]
+	public string? ConfirmPassword { set; get; }
 }
+
