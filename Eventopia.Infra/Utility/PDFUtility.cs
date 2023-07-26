9dc88370f01f432f56464c66bb2f639baad339a3
SumWutChill <mramzey@ymail.com>
2023-07-26 13:43:44 +0300

    Added Email and PDF functionality: 1. Added PDFUtility class in Infra 2. Added EmailUtitlity class in Infra 3. Added MailKit and PdfSharpCore packages to Infra layer

---
+++ Eventopia.Infra/Utility/PDFUtility.cs
@@ -0,0 +1,31 @@
+﻿using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventopia.Infra.Utility
+{
+	public static class PDFUtility
+	{
+		public static byte[] GeneratePdf(string body)
+		{
+			using (var stream = new MemoryStream())
+			{
+				var document = new PdfDocument();
+				var page = document.AddPage();
+
+				var font = new XFont("Arial", 12);
+				var gfx = XGraphics.FromPdfPage(page);
+
+				var xRect = new XRect(10, 50, page.Width.Point, 0);
+				gfx.DrawString(body, font, XBrushes.Black, xRect, XStringFormats.TopLeft);
+
+				document.Save(stream);
+				return stream.ToArray();
+			}
+		}
+	}
+}
